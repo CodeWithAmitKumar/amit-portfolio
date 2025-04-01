acdd96d6fe395c31e22eee097829a41e017fe23f
codeWithAmitKumar <work.amitpatra@gmail.com>
2025-04-01 22:13:40 +0530

    adding initial files

---
+++ src/assets/index.js
@@ -0,0 +1,39 @@
+import firstTestimonial from './Tars Ken.webp';
+import backend from "./backend.png";
+import close from "./close.svg";
+import creator from "./creator.png";
+import gearXpert from './gearXpert.png';
+import github from "./github.png";
+import secondTestimonial from './image.webp';
+import logo from "./logo.png";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
+import project2 from './proj1.webp';
+import project3 from './project3.webp';
+import aws from './tech/aws.png';
+import css from "./tech/css.png";
+import express from './tech/express.png';
+import figma from "./tech/figma.png";
+import framer from './tech/framer.png';
+import git from "./tech/git.png";
+import gsap from './tech/gsap.png';
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import mui from './tech/mui.png';
+import mysql from './tech/mysql.png';
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import thirdTestimonial from './third testimonial.webp';
+import web from "./web.png";
+
+export {
+  aws, backend, close, creator, css, express, figma, firstTestimonial, framer, gearXpert, git, github, gsap, html,
+  javascript, logo, menu, mobile, mongodb, mui, mysql, nodejs, project2,
+  project3, reactjs,
+  redux, secondTestimonial, tailwind, thirdTestimonial, threejs, web
+};
+
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
+      <a
+        href="https://www.instagram.com/thatodiapila/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram size={24} />
+      </a>
+      <a href="https://x.com/Codewith_amit?t=wzxyQYtIqyK_JnFzeww4uQ&s=09" target="_blank" rel="noopener noreferrer">
+        <FaXTwitter size={24} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/amit-web-developer/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={24} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
